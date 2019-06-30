4c8aeba5dbac84260d8d465bd3266efae3766244
Reuben <reuben.eggar@me.com>
2019-06-30 16:21:02 +0100

    Update linear.js

--- packages/vx-scale/src/scales/linear.js
+++ packages/vx-scale/src/scales/linear.js
@@ -7,7 +7,7 @@ export default ({ range, rangeRound, domain, nice = false, clamp = false }) => {
   if (range) scale.range(range);
   if (rangeRound) scale.rangeRound(rangeRound);
   if (domain) scale.domain(domain);
-  if (nice) scale.nice(nice);
+  if (nice) scale.nice(isArray(nice) ? nice : null);
   if (clamp) scale.clamp(true);
 
   return scale;
