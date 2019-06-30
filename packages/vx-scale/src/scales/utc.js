c7756ccfa86dd9510dc70689cce01c6cda35a090
Reuben <reuben.eggar@me.com>
2019-06-30 16:27:34 +0100

    Revert "Update utc.js"
    This reverts commit c671c1ab45eeaa73e68f3d23b8a2105f297fbee5.

--- packages/vx-scale/src/scales/utc.js
+++ packages/vx-scale/src/scales/utc.js
@@ -7,7 +7,7 @@ export default ({ range, rangeRound, domain, nice = false, clamp = false }) => {
   if (range) scale.range(range);
   if (rangeRound) scale.rangeRound(rangeRound);
   if (domain) scale.domain(domain);
-  if (nice) scale.nice(nice);
+  if (nice) scale.nice();
   if (clamp) scale.clamp(true);
 
   return scale;
